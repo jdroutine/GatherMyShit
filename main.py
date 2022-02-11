d8ce800e9d424b08719e419c00c138487e0e2b73
jdroutine <jd.routine@gmail.com>
2022-02-11 22:45:49 +0100

    dodanie gracza i koordynatów

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ import pygame
 # zainicjowanie biblioteki
 pygame.init()
 
+# koordynaty
+x = 0
+y = 0
+
+# gracz
+
+# tworzy prostokąt w miejscu x,y o wymiarach 100x100px
+player = pygame.rect.Rect(x, y, 100, 100)
+
 # stworzenie okna gry
 window = pygame.display.set_mode((800, 600))
 
@@ -14,5 +23,5 @@ while run:
         if event.type == pygame.QUIT:  # jeśli gracz zamknie okno gry
             run = False
 
-    window.fill((102, 0, 102)) # zmiana tła, kolor podany w rgb
-    pygame.display.update() # zapisuje każdą narysowaną zmianę
+    window.fill((102, 0, 102))  # zmiana tła, kolor podany w rgb
+    pygame.display.update()  # zapisuje każdą narysowaną zmianę
