cf0366fe3640e5f0bcd47f4ed9e62ab85de77967
jdroutine <jd.routine@gmail.com>
2022-02-12 20:16:35 +0100

    dodano poruszanie się postacią

--- GatherMyShit/main.py
+++ GatherMyShit/main.py
@@ -4,8 +4,8 @@ import pygame
 pygame.init()
 
 # koordynaty
-x = 0
-y = 0
+x = 70
+y = 50
 
 # gracz
 
@@ -18,11 +18,32 @@ window = pygame.display.set_mode((800, 600))
 # stworzenie nieskończonej pętli
 run = True
 while run:
-    # oczytywanie zdazreń wywołanych przez gracza
+
+    # szybkośc wykonywania pętli w fps
+    pygame.time.Clock().tick(60)
+
+    # odczytywanie zdarzeń wywołanych przez gracza
     for event in pygame.event.get():
         if event.type == pygame.QUIT:  # jeśli gracz zamknie okno gry
             run = False
 
+    keys = pygame.key.get_pressed()  # przechwycenie informacji z klawiatury
+
+    speed = 5  # prędkość poruszania się
+
+    # sprawdzamy czy klawisz (strzałka w prawo)  jest naciśnięty
+    if keys[pygame.K_RIGHT]:
+        x += speed
+    if keys[pygame.K_LEFT]:  # strzałka w lewo
+        x -= speed
+    if keys[pygame.K_UP]:  # strzałka w górę
+        y -= speed
+    if keys[pygame.K_DOWN]:  # strzałka w dół
+        y += speed
+
+    player = pygame.rect.Rect(x, y, 100, 100)  # odświerzenie położenia postaci
+
     window.fill((102, 0, 102))  # zmiana tła, kolor podany w rgb
-    pygame.draw.rect(window,(255,255,255),player) # narysowanie gracza (okno,kolor,gracz)
+    # narysowanie gracza (okno,kolor,gracz)
+    pygame.draw.rect(window, (255, 255, 255), player)
     pygame.display.update()  # zapisuje każdą narysowaną zmianę
